475cefae304c33dd2607497f1e99df7403526f22
Nityam24 <nityampatel2402@gmail.com>
2023-01-31 01:02:43 +0530

    updated

--- contact.js
+++
@@ -1,7 +0,0 @@
-var form = document.getElementById("form");
-
-      form.addEventListener('submit', function(e){ 
-        e.preventDefault();
-         var alert = document.getElementById("alert");
-         alert.innerHTML="Your message has been deliver successfully";
-      })
